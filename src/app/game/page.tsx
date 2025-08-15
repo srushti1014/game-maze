373a5075d505b770cf57e05a03a438dcc43efa15
srushti <srushtipatel4040@gmail.com>
2025-08-15 16:26:20 +0530

    new1

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,8 +2,10 @@
 import MazeMaker from "@/components/MazeMaker";
 import { Cell, MazeBoard } from "@/utils/mazeboard";
 import { usePlayer } from "@/components/usePlayer";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
+import { IoMdTimer } from "react-icons/io";
+import { FaArrowRight, FaStar, FaTrophy } from "react-icons/fa";
 
 // const cellSize = 30;
 
@@ -16,6 +18,9 @@ export default function MazePage() {
   const [level, setLevel] = useState(1);
   const [showPrompt, setShowPrompt] = useState(false);
   const [cellSize, setCellSize] = useState(30);
+  const [time, setTime] = useState(0);
+  const [score, setScore] = useState(0)
+  const timeRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -41,9 +46,14 @@ export default function MazePage() {
   }, [mazeGrid]);
 
   const handleWin = () => {
+
+    if (timeRef.current) clearInterval(timeRef.current);
+    const gainedScore = getScoreForLevel();
+    setScore(prev => prev + gainedScore);
     setShowPrompt(true);
     toast.success(`You won this level ${level}!`);
   };
+
   const player = usePlayer(mazeGrid, rows, cols, handleWin);
 
   useEffect(() => {
@@ -69,6 +79,21 @@ export default function MazePage() {
   }, [difficulty]);
 
   useEffect(() => {
+    if (difficulty) {
+      setTime(0);
+      if (timeRef.current) clearInterval(timeRef.current);
+      timeRef.current = setInterval(() => {
+        setTime(prev => prev + 1)
+      }, 1000);
+    }
+
+    return () => {
+      if (timeRef.current) clearInterval(timeRef.current)
+    }
+  }, [difficulty, level])
+
+
+  useEffect(() => {
     if (rows && cols) {
       const newRows = rows + (level - 1) * growthStep;
       const newCols = cols + (level - 1) * growthStep;
@@ -82,11 +107,25 @@ export default function MazePage() {
   const nextLevel = () => {
     setLevel(prev => prev + 1);
     setShowPrompt(false);
+    setTime(0);
+
+    if (timeRef.current) clearInterval(timeRef.current);
+    timeRef.current = setInterval(() => {
+      setTime(prev => prev + 1);
+    }, 1000);
   };
 
+  const getScoreForLevel = () => {
+    let baseScore: number = 0;
+    if (difficulty === "easy") baseScore = 100;
+    if (difficulty === "medium") baseScore = 200;
+    if (difficulty === "hard") baseScore = 300;
+    if (difficulty === "superhard") baseScore = 500;
 
-
-
+    return Math.max(
+      baseScore - Math.floor(
+        time * (difficulty === "easy" ? 1 : difficulty === "medium" ? 2 : difficulty === "hard" ? 3 : 4)), 10);
+  };
 
 
   return (
@@ -121,17 +160,52 @@ export default function MazePage() {
               <MazeMaker mazeGrid={mazeGrid} player={player} cellSize={cellSize} />
             )}
           </div>
+
+          <div className="flex flex-col justify-center items-center max-w-lg mx-auto text-xl mt-8">
+            <p>Time: {time}</p>
+            <p>Score: {score} </p>
+          </div>
         </>
       )}
 
       {showPrompt && (
-        <div className="fixed inset-0 bg-black/80 bg-opacity-50 flex items-center justify-center">
-          <div className="bg-[#922668] text-white px-8 py-6 rounded-lg border border-[#38bdf8] flex flex-col items-center gap-4">
-            <h3 className="text-2xl font-bold">Level {level} Complete!</h3>
+        <div className="fixed inset-0 bg-black/80 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-[#922668] text-white px-8 py-6 rounded-lg border-2 border-[#38bdf8] flex flex-col items-center max-w-sm w-full mx-4">
+            <div className="text-center mb-4">
+              <h3 className="text-2xl font-bold mb-2">Level {level} Complete!</h3>
+            </div>
+
+            <div className="w-full space-y-3 mb-4">
+              <div className="flex items-center justify-between bg-[#7a1d55]/50 px-4 py-3 rounded-lg">
+                <div className="flex items-center gap-3">
+                  <IoMdTimer size={24} className="text-green-500 flex-shrink-0" />
+                  <span>Time Taken :</span>
+                </div>
+                <span className="font-medium">{time}s</span>
+              </div>
+
+              <div className="flex items-center justify-between bg-[#7a1d55]/50 px-4 py-3 rounded-lg">
+                <div className="flex items-center gap-3">
+                  <FaStar size={20} className="text-yellow-300 flex-shrink-0" />
+                  <span>Score Gained :</span>
+                </div>
+                <span className="font-medium">{getScoreForLevel()}</span>
+              </div>
+
+              <div className="flex items-center justify-between bg-[#7a1d55]/50 px-4 py-3 rounded-lg">
+                <div className="flex items-center gap-3">
+                  <FaTrophy size={20} className="text-blue-300 flex-shrink-0" />
+                  <span>Total Score :</span>
+                </div>
+                <span className="font-medium">{score}</span>
+              </div>
+            </div>
+
             <button
               onClick={nextLevel}
-              className="bg-[#38bdf8] text-white px-6 py-2 rounded-md hover:bg-[#0ea5e9] transition-colors"
+              className="bg-[#36b9f1] text-white px-6 py-2 rounded-md hover:bg-[#0ea5e9] transition-colors mt-2 flex items-center gap-2 font-medium cursor-pointer"
             >
+              <FaArrowRight size={16} />
               Next Level
             </button>
           </div>
